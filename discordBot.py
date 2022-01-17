b27e5afabd7abff7b1bdf822ff533a371114381c
Maartin <maartinschultz@gmail.com>
2022-01-17 20:21:51 +0100

    correction decorateur bot Discord

--- discordBot.py
+++ discordBot.py
@@ -29,18 +29,19 @@ pyBotSon = PyBot()
 
 pyBotSon = commands.Bot(command_prefix="!")
 
-@pyBotSon.event         
+@pyBotSon.event
 async def on_ready():
         print(f'{pyBotSon.user.display_name} has connected to Discord!')
 
+@pyBotSon.listen()
 async def on_message(message):
     #print(message.content)
     if message.content.lower()=="hello":
         if message.author.id ==220617008436346880:
             await message.channel.send("Bonjour Père")
         else:
-            await message.channel.send("Bonjour "+ message.author.display_name)    
-        
+            await message.channel.send("Bonjour "+ message.author.display_name)
+
 class PyBotCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -50,7 +51,7 @@ class PyBotCommand(commands.Cog):
         await ctx.send("test OK")
     
     @commands.command(name='del')
-    async def dele(self, ctx, number_of_messages: int):#ctx contexte #number_of_messages -> paramètre n°1 de type int
+    async def delete(self, ctx, number_of_messages: int):#ctx contexte #number_of_messages -> paramètre n°1 de type int
         messages = await ctx.channel.history(limit=number_of_messages + 1).flatten()
         for each_message in messages:
             await each_message.delete()
@@ -60,6 +61,11 @@ class PyBotCommand(commands.Cog):
         r = requests.get('https://www.cyanidevf.fr/image-aleatoire/')
         image = re.findall('src=\"(https://www.cyanidevf.fr/wp-content/uploads/[0-9]*/[0-9]*/[a-z]*[-]?[0-9]?[.]png)',str(r.content))[0]
         await ctx.send(image)
+        
+    @commands.command(name='')
+    async def fonction(self, ctx):
+        
+        await ctx.send("en maintenance")
 
 
 pyBotSon.add_cog(PyBotCommand(pyBotSon))
